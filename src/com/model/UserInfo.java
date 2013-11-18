f546a10a5065b5efb38857b47e00633edeae821d
zhoaruisong <zhaoruisong@163.com>
2013-11-19 00:19:46 +0800

    添加logo画面等，添加进度条

---
+++ src/com/model/UserInfo.java
@@ -0,0 +1,46 @@
+package com.model;
+
+public class UserInfo {
+	
+	private String strUserId;
+	
+	private String strUserName;
+	
+	private String strUserPwd;
+	
+	private String strMemu;
+
+	
+	public String getStrUserId() {
+		return strUserId;
+	}
+
+	public void setStrUserId(String strUserId) {
+		this.strUserId = strUserId;
+	}
+
+	public String getStrUserName() {
+		return strUserName;
+	}
+
+	public void setStrUserName(String strUserName) {
+		this.strUserName = strUserName;
+	}
+
+	public String getStrUserPwd() {
+		return strUserPwd;
+	}
+
+	public void setStrUserPwd(String strUserPwd) {
+		this.strUserPwd = strUserPwd;
+	}
+
+	public String getStrMemu() {
+		return strMemu;
+	}
+
+	public void setStrMemu(String strMemu) {
+		this.strMemu = strMemu;
+	}
+	
+}
