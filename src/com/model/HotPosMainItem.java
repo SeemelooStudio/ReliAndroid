44fc09d0f362d3eebbe75af3f9eab5cfb33b66e4
zhoaruisong <zhaoruisong@163.com>
2013-11-22 21:37:01 +0800

    温度曲线图参数添加

---
+++ src/com/model/HotPosMainItem.java
@@ -0,0 +1,65 @@
+package com.model;
+
+public class HotPosMainItem {
+
+	private String hotPosId;
+	
+	private String title;
+	
+	private String wenduLeft;
+	
+	private String wenduLeftPa;
+	
+	private String wenduRight;
+	
+	private String wenduRightPa;
+
+	public String getHotPosId() {
+		return hotPosId;
+	}
+
+	public void setHotPosId(String hotPosId) {
+		this.hotPosId = hotPosId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getWenduLeft() {
+		return wenduLeft;
+	}
+
+	public void setWenduLeft(String wenduLeft) {
+		this.wenduLeft = wenduLeft;
+	}
+
+	public String getWenduLeftPa() {
+		return wenduLeftPa;
+	}
+
+	public void setWenduLeftPa(String wenduLeftPa) {
+		this.wenduLeftPa = wenduLeftPa;
+	}
+
+	public String getWenduRight() {
+		return wenduRight;
+	}
+
+	public void setWenduRight(String wenduRight) {
+		this.wenduRight = wenduRight;
+	}
+
+	public String getWenduRightPa() {
+		return wenduRightPa;
+	}
+
+	public void setWenduRightPa(String wenduRightPa) {
+		this.wenduRightPa = wenduRightPa;
+	}
+
+}
