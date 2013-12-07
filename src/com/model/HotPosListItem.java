eec5f5e2be4efc9c91621a54973ca35ea1cd3a79
zhoaruisong <zhaoruisong@163.com>
2013-12-07 22:20:56 +0800

    add poslistitem

---
+++ src/com/model/HotPosListItem.java
@@ -0,0 +1,55 @@
+package com.model;
+
+public class HotPosListItem {
+
+	private String strStationId;
+	
+	private String strStationName;
+	
+	private String strWarnColor;
+	
+	private String strAddress;
+	
+	private String strDirect;
+
+	public String getStrStationId() {
+		return strStationId;
+	}
+
+	public void setStrStationId(String strStationId) {
+		this.strStationId = strStationId;
+	}
+
+	public String getStrStationName() {
+		return strStationName;
+	}
+
+	public void setStrStationName(String strStationName) {
+		this.strStationName = strStationName;
+	}
+
+	public String getStrWarnColor() {
+		return strWarnColor;
+	}
+
+	public void setStrWarnColor(String strWarnColor) {
+		this.strWarnColor = strWarnColor;
+	}
+
+	public String getStrAddress() {
+		return strAddress;
+	}
+
+	public void setStrAddress(String strAddress) {
+		this.strAddress = strAddress;
+	}
+
+	public String getStrDirect() {
+		return strDirect;
+	}
+
+	public void setStrDirect(String strDirect) {
+		this.strDirect = strDirect;
+	}
+
+}
